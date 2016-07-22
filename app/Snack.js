bd340c1d865e07fcdc1a4efdabede833b33f8922
Gerardo Arevalo <gerardo@atctech.co>
2016-07-21 21:52:16 -0400

    WIP

--- app/Snack.js
+++ app/Snack.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { DragSource } from 'react-dnd';
-
 // snack Drag'nDrop spec
 //
 // - Required: beginDrag
@@ -21,20 +20,39 @@ const snackSpec = {
         }
     }
 };
-
+// Snack DragSource collect collecting function.
+// - connect: An instance of DragSourceConnector.
+// You use it to assign the drag source role to a DOM node.
+//
+// - monitor: An instance of DragSourceMonitor.
+// You use it to connect state from the React DnD to your component’s properties.
+// Available functions to get state include canDrag(), isDragging(), getItemType(),
+// getItem(), didDrop() etc.
+let collect = (connect, monitor) => {
+    return {
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+    };
+}
 class Snack extends Component {
     render() {
-        const { name } = this.props;
+        const { name, isDragging, connectDragSource } = this.props;
+        const opacity = isDragging ? 0.4 : 1;
         const style = {
-            opacity: 1
+            opacity: opacity
         };
         return (
-            <div className='snack' style={style}>
-                {name}
-            </div>
-        )
+            connectDragSource(
+                <div className='snack' style={style}>
+                    {name}
+                </div>
+            )
+        );
     }
 }
 Snack.propTypes = {
+    connectDragSource: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired,
     name: PropTypes.string.isRequired
 };
+export default DragSource('snack', snackSpec, collect)(Snack);
