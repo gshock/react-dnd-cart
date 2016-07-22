02f392b9bc918c76bc7c946e45affe8dfa1d8970
Gerardo Arevalo <gerardo@atctech.co>
2016-07-21 21:44:59 -0400

    WIP

--- app/Snack.js
+++ app/Snack.js
@@ -1,5 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 import { DragSource } from 'react-dnd';
+
+// snack Drag'nDrop spec
+//
+// - Required: beginDrag
+// - Optional: endDrag
+// - Optional: canDrag
+// - Optional: isDragging
+const snackSpec = {
+    beginDrag(props) {
+        return {
+            name: props.name
+        };
+    },
+    endDrag(props, monitor) {
+        const dragItem = monitor.getItem();
+        const dropResult = monitor.getDropResult();
+        if (dropResult) {
+            console.log(`You dropped ${dragItem.name} into ${dropResult.name}`);
+        }
+    }
+};
+
 class Snack extends Component {
     render() {
         const { name } = this.props;
